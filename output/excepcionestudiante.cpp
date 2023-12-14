602e0683d6b1d5a67679fb8fa799f9bff09179a4
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-14 11:54:43 +0000

    excepcion materia

--- output/excepcionestudiante.cpp
+++ output/excepcionestudiante.cpp
@@ -1,8 +1,16 @@
 #include <stdexcept>
+#include <iostream>
 
 class FechaInvalidaException : public std::exception {
 public:
     const char* what() const throw() {
         return "Error: Fecha inválida.";
+    }
+};
+
+class MateriaNoRegistradaException : public std::exception {
+public:
+    const char* what() const throw() {
+        return "Error: Materia no registrada.";
     }
 };
