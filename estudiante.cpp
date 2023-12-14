b79e7b3d238767e0d5c975461d838af8c7122e27
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-14 11:27:40 +0000

    cambio de orden

--- estudiante.cpp
+++ estudiante.cpp
@@ -3,33 +3,33 @@
 #include <string>
 #include <vector>
 
+struct Materia {
+    std::string nombre;
+};
+
 class Estudiante {
-    public:
+public:
     std::string nombre;
     int edad;
     float promedio;
     std::vector<Materia> materias;
 
-void mostrarEstudiante() const {
-    std::cout << "Nombre: " << nombre << std::endl;
-    std::cout << "Edad: " << edad << std::endl;
-    std::cout << "Promedio: " << promedio << std::endl;
-}
+    void mostrarEstudiante() const {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Promedio: " << promedio << std::endl;
+    }
 
-struct Materia {
-    std::string nombre;
-}
+    void agregarMateria(const Materia& materia) {
+        materias.push_back(materia);
+    }
 
-void agregarMateria(const Materia& materia) {
-    materias.push_back(materia);
-}
-
-void eliminarMateria(const std::string& nombreMateria) {
-    for (int i = 0; i < materias.size(); i++) {
-        if (materias[i].nombre == nombreMateria) {
-            materias.erase(materias.begin() + i);
-            break;
+    void eliminarMateria(const std::string& nombreMateria) {
+        for (int i = 0; i < materias.size(); i++) {
+            if (materias[i].nombre == nombreMateria) {
+                materias.erase(materias.begin() + i);
+                break;
+            }
         }
     }
-}
 };
