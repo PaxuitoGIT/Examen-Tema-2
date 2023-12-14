fc5851708d48398886da9bca5b108edde9d3c2bd
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-14 11:41:45 +0000

    eliminar y mostrar materia c

--- estudiante.c
+++ estudiante.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_MATERIAS 50
+#define MAX_MATERIAS 10
 
 struct Materia {
     char nombre[50];
-}
+};
 
 struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
+    struct Materia materias[MAX_MATERIAS];
+    int numMaterias;
 };
 
 void mostrarEstudiante(struct Estudiante estudiante) {
@@ -26,5 +28,29 @@ void agregarMateria(struct Estudiante* estudiante, const struct Materia* materia
         estudiante->numMaterias++;
     } else {
         printf("Error: No se pueden agregar más materias\n");
+    }
+}
+
+void eliminarMateria(struct Estudiante* estudiante, const char* nombreMateria) {
+    int i;
+    for (i = 0; i < estudiante->numMaterias; i++) {
+        if (strcmp(estudiante->materias[i].nombre, nombreMateria) == 0) {
+            break;
+        }
+    }
+
+    if (i < estudiante->numMaterias) {
+        for (int j = i; j < estudiante->numMaterias - 1; j++) {
+            estudiante->materias[j] = estudiante->materias[j + 1];
+        }
+        estudiante->numMaterias--;
+    } else {
+        printf("Error: No se encontró la materia\n");
+    }
+}
+
+void mostrarMaterias(struct Estudiante* estudiante) {
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        printf("Materia: %s\n", estudiante->materias[i].nombre);
     }
 }
