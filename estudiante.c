b56a6ea1fc83c63070b366441c72d447cfea580d
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-14 11:51:21 +0000

    asistencia en c

--- estudiante.c
+++ estudiante.c
@@ -3,17 +3,27 @@
 #include <string.h>
 
 #define MAX_MATERIAS 10
+#define MAX_ASISTENCIAS 100
 
 struct Materia {
     char nombre[50];
 };
+
+struct Asistencia {
+    char fecha[20];
+    char materia[50];
+    char estado[10];
+};
+
 
 struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
     struct Materia materias[MAX_MATERIAS];
+    struct Asistencia asistencias[MAX_ASISTENCIAS];
     int numMaterias;
+    int numAsistencias;
 };
 
 void mostrarEstudiante(struct Estudiante estudiante) {
@@ -52,5 +62,20 @@ void eliminarMateria(struct Estudiante* estudiante, const char* nombreMateria) {
 void mostrarMaterias(struct Estudiante* estudiante) {
     for (int i = 0; i < estudiante->numMaterias; ++i) {
         printf("Materia: %s\n", estudiante->materias[i].nombre);
+    }
+}
+
+void registrarAsistencia(struct Estudiante* estudiante, const struct Asistencia* asistencia) {
+    if (estudiante->numAsistencias < MAX_ASISTENCIAS) {
+        estudiante->asistencias[estudiante->numAsistencias] = *asistencia;
+        estudiante->numAsistencias++;
+    } else {
+        printf("Error: No se pueden registrar más asistencias\n");
+    }
+}
+
+void mostrarAsistencias(struct Estudiante* estudiante) {
+    for (int i = 0; i < estudiante->numAsistencias; ++i) {
+        printf("Fecha: %s, Materia: %s, Estado: %s\n", estudiante->asistencias[i].fecha, estudiante->asistencias[i].materia, estudiante->asistencias[i].estado);
     }
 }
